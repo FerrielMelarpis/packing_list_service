341369d6631bdb47b49a7b420a429cca4b694429
FerrielMelarpis <fbmelarpis@proton.me>
2024-12-21 17:27:20 +0800

    added packing-list service

--- src/packing-list/packing-list.module.ts
+++ src/packing-list/packing-list.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PackingListController } from './packing-list.controller';
 import { PackingListService } from './packing-list.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PackingList } from './entities/packing-list.entity';
+import { PackingItem } from './entities/packing-item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PackingList, PackingItem])],
   controllers: [PackingListController],
   providers: [PackingListService],
 })
-export class PackingListModule { }
+export class PackingListModule {}
--- src/packing-list/packing-list.service.ts
+++ src/packing-list/packing-list.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PackingList } from './entities/packing-list.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class PackingListService {}
+export class PackingListService {
+  constructor(
+    @InjectRepository(PackingList)
+    private packingListRepository: Repository<PackingList>,
+  ) {}
+
+  createPackingList = (name: string): Promise<PackingList> => {
+    const packingList = this.packingListRepository.create({ name });
+    return this.packingListRepository.save(packingList);
+  };
+
+  findAll = (): Promise<PackingList[]> => {
+    return this.packingListRepository.find({
+      relations: {
+        items: true,
+      },
+    });
+  };
+
+  findOne = (id: string): Promise<PackingList> => {
+    return this.packingListRepository.findOne({
+      where: { id },
+      relations: ['items'],
+    });
+  };
+
+  updatePackingList = async (id: string, updates: Partial<PackingList>) => {
+    const packingList = await this.findOne(id);
+
+    if (packingList) {
+      Object.assign(packingList, updates);
+      return this.packingListRepository.save(packingList);
+    }
+
+    return null;
+  };
+
+  deletePackingList = async (id: string) => {
+    const result = await this.packingListRepository.delete(id);
+    return result.affected;
+  };
+}
