b579fb0dbac6f454fd2a89a4a9089bf55c9372f3
FerrielMelarpis <fbmelarpis@proton.me>
2024-12-21 16:36:02 +0800

    init nestjs app

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PackingListModule } from './packing-list/packing-list.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/packing-list.sqlite',
+      entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      synchronize: process.env.NODE_ENV == 'dev',
+    }),
+    PackingListModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
